15a9d5dad89eef1e9e8d0c48fed17daf89e17ddb
afterain71 <afterain71@gmail.com>
2018-06-23 16:54:21 +0300

    add VM

---
+++ VM/assembler.py
@@ -0,0 +1,141 @@
+from common import *
+import sys
+import numpy as np
+
+
+def parse_command(line, arg1_type=None, arg2_type=None):
+    arg1 = 0
+    if arg1_type is None:
+        return [0, 0]
+    elif arg1_type == "reg":
+        arg1 = registers.get(line[1])
+        Check(arg1 is not None, "wrong argument " + line[1])
+    elif arg1_type == "line":
+        try:
+            arg1 = (int(line[1]) - 1) * COMMAND_LEN + FIRST_COMMAND_ADDR
+        except ValueError:
+            Check(False, "wrong argument " + line[1])
+    arg2 = 0
+    if arg2_type is None:
+        pass
+    elif arg2_type == "reg":
+        arg2 = registers.get(line[2])
+        Check(arg2 is not None, "wrong argument " + line[2])
+    elif arg2_type == "int":
+        try:
+            arg2 = int(line[2])
+        except ValueError:
+            Check(False, "wrong argument " + line[2])
+    return [arg1, arg2]
+
+
+def translate_labels(path_in):
+    with open(path_in, mode='r') as command_file:
+        labels = []
+        pos = 1
+        for line in command_file:
+            if line.startswith("label"):
+                line = line.strip(":\n").split(' ')[1]
+                labels.append((line, pos))
+                continue
+            pos += 1
+        return dict(labels)
+
+
+def make_array_from_asm(path_in):
+    labels = translate_labels(path_in)
+    with open(path_in, mode='r', encoding='utf-8') as command_file:
+        output = []
+        output += [0] * len(registers)
+        output[EIP] = FIRST_COMMAND_ADDR
+        statics = []
+        statics_pos = 0
+
+        for line in command_file:
+            if line.startswith("label"):
+                continue
+            line = line.split(" ;", 1)[0]
+            print_static = False
+            if line.startswith("print \""):
+                line = line.strip('\n').split('\"')[:-1]
+                line[0] = "print"
+                print_static = True
+            else:
+                line = line.strip('\n').split(' ')
+
+            command_id = commands.get(line[0])
+            output.extend([command_id])
+            if command_id == MOV:
+                output.extend(parse_command(line, "reg", "reg"))
+            elif command_id == MOVN:
+                output.extend(parse_command(line, "reg", "int"))
+            elif command_id == ADD:
+                output.extend(parse_command(line, "reg", "reg"))
+            elif command_id == ADDN:
+                output.extend(parse_command(line, "reg", "int"))
+            elif command_id == SUB:
+                output.extend(parse_command(line, "reg", "reg"))
+            elif command_id == SUBN:
+                output.extend(parse_command(line, "reg", "int"))
+            elif command_id == JMP:
+                line[1] = labels.get(line[1], line[1])
+                output.extend(parse_command(line, "line"))
+            elif command_id == JLZ:
+                line[1] = labels.get(line[1], line[1])
+                output.extend(parse_command(line, "line", "reg"))
+            elif command_id == PUSH:
+                output.extend(parse_command(line, "reg"))
+            elif command_id == POP:
+                output.extend(parse_command(line, "reg"))
+            elif command_id == CALL:
+                line[1] = labels.get(line[1], line[1])
+                output.extend(parse_command(line, "line"))
+            elif command_id == RET:
+                output.extend(parse_command(line))
+            elif command_id == END:
+                output.extend(parse_command(line))
+            elif command_id == PRINT:
+                if print_static:
+                    output.extend([statics_pos, 1])
+                    encoded = list(line[1].encode('utf-8'))
+                    encoded.extend([0] * (4 - len(encoded) % 4))
+                    packed = np.zeros(len(encoded) // 4, dtype=np.uint)
+                    for i in np.arange(0, len(encoded), 4):
+                        packed[i//4] = ((encoded[i] << 24) +
+                                        (encoded[i + 1] << 16) +
+                                        (encoded[i + 2] << 8) +
+                                        (encoded[i + 3]))
+
+                    statics.append(len(packed))
+                    statics_pos += 1 + len(packed)
+                    statics.extend(packed.view(dtype=np.int))
+                else:
+                    output.extend(parse_command(line, "reg"))
+            elif command_id == READN:
+                output.extend(parse_command(line, "reg"))
+            else:
+                print("Error: " + line[0] + " is not a valid command")
+                return False
+
+        output[ESP] = len(output)
+        output[EBP] = len(output)
+        output.extend([0] * STACK_SIZE)
+        output[STC] = len(output)
+        output.extend(statics)
+        return output
+
+
+def write_array_as_bytes(array, path_out):
+        np.array(array, dtype=np.int).tofile(path_out)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) not in [2, 3]:
+        print("Usage: assembler.py <source file> [<dest file>]")
+        exit(-1)
+    output_path = str(sys.argv[1]) + ".btc"
+    if len(sys.argv) == 3:
+        output_path = sys.argv[2]
+
+    output_array = make_array_from_asm(sys.argv[1])
+    write_array_as_bytes(output_array, output_path)
---
+++ VM/machine.py
@@ -0,0 +1,171 @@
+import numpy as np
+import sys
+from common import *
+
+
+class BinaryFile:
+    def __init__(self, path):
+        self.path = path
+        self.buff = np.fromfile(path, dtype=np.int)
+        self.size = len(self.buff)
+
+    def write(self, address, data):
+        Check(address < self.size, "Attempt to write at address {0} when program size is {1}".format(address, self.size))
+        self.buff[address] = data
+
+    def read(self, address):
+        Check(address < self.size, "Attempt to read at address {0} when program size is {1}".format(address, self.size))
+        return self.buff[address]
+
+    def print(self, end=None):
+        for cell in np.arange(0, self.size if end is None else end, COMMAND_LEN):
+            print("[{0:3d}]".format(cell), end=' ')
+            print("{0:4d} {1:4d} {2:4d}".format(self.read(cell), self.read(cell + 1), self.read(cell + 2)))
+
+        print("")
+
+    def dump(self, path=None):
+        if path is None:
+            path = self.path
+        self.buff.tofile(path, dtype=np.int)
+
+
+class VirtualMachine:
+    def __init__(self, memory):
+        self.memory = memory
+        self.private_reg = [EIP, ESP, EBP, STC, NIU]
+
+    def Run(self):
+        while self.run_next_command():
+            pass
+
+    def mov(self, where, what):
+        self.movn(where, self.at(what))
+
+    def movn(self, where, what):
+        Check(where not in self.private_reg, "Attempt to write {1} to protected memory {0}".format(where, what))
+        self.memory.write(where, what)
+
+    def add(self, where, what):
+        self.addn(where, self.at(what))
+
+    def addn(self, where, what):
+        Check(where not in self.private_reg, "Attempt to write {1} to protected memory {0}".format(where, what))
+        result = self.at(where) + what
+        self.memory.write(where, result)
+
+    def sub(self, where, what):
+        self.addn(where, -self.at(what))
+
+    def subn(self, where, what):
+        self.addn(where, -what)
+
+    def jmp(self, where):
+        Check(where > EDX, "Attempt to execute data")
+        self.memory.write(EIP, where - COMMAND_LEN)
+
+    def jlz(self, where, why):
+        if self.at(why) < 0:
+            self.jmp(where)
+
+    def push(self, where):
+        self.memory.write(self.at(ESP), self.at(where))
+        self.memory.write(ESP, self.at(ESP) + 1)
+
+    def pop(self, where):
+        Check(self.at(ESP) >= self.at(EBP), "Poping from empty stack")
+        self.memory.write(ESP, self.at(ESP) - 1)
+        self.memory.write(where, self.at(self.at(ESP)))
+
+    def call(self, where):
+        self.push(EBP)
+        self.memory.write(EBP, self.at(ESP) - 1)
+        self.push(EIP)
+        self.memory.write(EIP, where - COMMAND_LEN)
+
+    def ret(self):
+        self.memory.write(ESP, self.at(EBP))
+        self.memory.write(EBP, self.at(self.at(EBP)))
+        self.memory.write(EIP, self.at(self.at(ESP) + 1))
+
+    def print(self, where, mode):
+        if mode == 1:  # static
+            start = self.at(STC) + where + 1
+            length = self.at(start - 1)
+            mask = [255 << 24, 255 << 16, 255 << 8, 255]
+            packed = [self.at(start + i) for i in range(length)]
+            packed = np.array(packed).view(np.uint)
+            encoded = []
+            for cell in packed:
+                encoded.extend([cell >> 24, (cell & mask[1]) >> 16, (cell & mask[2]) >> 8, cell & mask[3]])
+            print(bytes(encoded).decode('utf-8').strip(' '))
+        else:
+            print(self.at(where))
+
+    def readn(self, a1):
+        while True:
+            try:
+                i = int(input())
+                break
+            except ValueError:
+                print('That is not a number!')
+        self.memory.write(a1, i)
+
+    def run_next_command(self):
+        command = self.at(self.ip())
+        arg_1 = self.at(self.ip() + 1)
+        arg_2 = self.at(self.ip() + 2)
+
+        if command == MOV:
+            self.mov(arg_1, arg_2)
+        elif command == MOVN:
+            self.movn(arg_1, arg_2)
+        elif command == ADD:
+            self.add(arg_1, arg_2)
+        elif command == ADDN:
+            self.addn(arg_1, arg_2)
+        elif command == SUB:
+            self.sub(arg_1, arg_2)
+        elif command == SUBN:
+            self.subn(arg_1, arg_2)
+        elif command == JMP:
+            self.jmp(arg_1)
+        elif command == JLZ:
+            self.jlz(arg_1, arg_2)
+        elif command == PUSH:
+            self.push(arg_1)
+        elif command == POP:
+            self.pop(arg_1)
+        elif command == CALL:
+            self.call(arg_1)
+        elif command == RET:
+            self.ret()
+        elif command == END:
+            return False
+        elif command == PRINT:
+            self.print(arg_1, arg_2)
+        elif command == READN:
+            self.readn(arg_1)
+        else:
+            print("Error: ", command, " is not a valid command code")
+            return False
+
+        self.memory.write(EIP, self.ip() + COMMAND_LEN)
+        if DEBUG:
+            self.memory.print(100)
+        return True
+
+    def ip(self):
+        return self.memory.read(EIP)
+
+    def at(self, addr):
+        return self.memory.read(addr)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: machine.py <.btc file>")
+        exit(-1)
+
+    vm = VirtualMachine(memory=BinaryFile(sys.argv[1]))
+    vm.Run()
